76ce4dc9c809cf94fee3e9dd0c8c2d756b33d834
Yaël Balbastre <yael.balbastre@gmail.com>
2026-07-15 13:37:29 +0200

    Update import in test for template package

--- tests/test_import.py
+++ tests/test_import.py
@@ -5,5 +5,5 @@ import importlib
 
 def test_template_submodule_is_importable() -> None:
     """The template package should be importable after installation."""
-    module = importlib.import_module("bagof.things")
+    module = importlib.import_module("griffe_typing_extensions")
     assert module is not None
